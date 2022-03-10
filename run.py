83b762ba1f7faf58e1080ba8c63561eff2543747
Clayton McDonald <98728475+cmcdo@users.noreply.github.com>
2022-03-10 22:29:37 +1100

    Add files via upload

--- run.py
+++ run.py
@@ -3,28 +3,44 @@ import os
 from os import walk
 from pynput.keyboard import Key, Controller
 import time
+
 keyboard = Controller()
 key = "f5"
-if not ("chrome.exe" in (i.name() for i in psutil.process_iter())):
-    for file in [f for f in os.listdir('.') if os.path.isfile(f)]:
-        if ".pdf" in file:
-            os.startfile(file)
-            time.sleep(0.1)
-            os.startfile("refresh.vbs")
-else:
-    os.startfile("refresh.vbs")
-    time.sleep(0.1)
-    
-    keyboard.press(Key.ctrl)
-    keyboard.press("1")
-    
-    keyboard.release(Key.ctrl)
-    keyboard.release("1")
+#Finding out what file
+FileType = ""
+FileName = ''
+for file in [f for f in os.listdir('.') if os.path.isfile(f)]:
+    if '.tex' in file:
+        FileType = 'Latex'
+        FileName = file
+        break
+    elif '.py' in file and file != 'run.py':
+        FileType = 'Python'
+        FileName = file
+        break
+        
 
-    time.sleep(0.1)
-    keyboard.press(Key.f5)
+if FileType == 'Latex':
+    os.system('cmd /c "pdflatex "'+FileName)
+    if not ("chrome.exe" in (i.name() for i in psutil.process_iter())):
+        os.startfile(FileName)
+        time.sleep(0.1)
+        os.startfile("refresh.vbs")
 
-    keyboard.release(Key.f5)
-    time.sleep(0.1)
-    keyboard.press(Key.f11)
-    keyboard.release(Key.f11)
+    else:
+        os.startfile("refresh.vbs")
+        time.sleep(0.1)
+        
+        keyboard.press(Key.ctrl)
+        keyboard.press("1")
+        
+        keyboard.release(Key.ctrl)
+        keyboard.release("1")
+
+        time.sleep(0.1)
+        keyboard.press(Key.f5)
+
+        keyboard.release(Key.f5)
+        time.sleep(0.1)
+        keyboard.press(Key.f11)
+        keyboard.release(Key.f11)
